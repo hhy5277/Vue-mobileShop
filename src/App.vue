6c8e8a69d8afe91ca024def91f63b29ce8bc0b89
lxjay <lxjay@163.com>
2018-05-13 15:28:36 +0800

    更新过渡动画

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
-    <router-view/>
+    <transition name='fade' mode="out-in">
+        <router-view/>
+    </transition>
+    <tabs></tabs>
   </div>
 </template>
 
 <script>
+import tabs from "@/components/component/tabs";
 export default {
   name: "App",
+  components: {
+    tabs
+  }
 };
 </script>
 
 <style>
 #app {
   background-color: #f0f0f0;
+  padding-bottom: 2.8rem;
+}
+.fade-enter {
+  opacity: 0;
+}
+.fade-leave {
+  opacity: 1;
+}
+.fade-enter-active {
+  -webkit-transition: opacity 0.5s;
+}
+.fade-leave-active {
+  opacity: 0;
+  -webkit-transition: opacity 0.5s;
 }
 </style>
