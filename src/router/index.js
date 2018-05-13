e6f8d1d66f6fca55a1b70d1cb830f577359baf9e
lxjay <lxjay@163.com>
2018-05-13 15:30:34 +0800

    新增路由跳转

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ShoppingMall from '@/components/pages/ShoppingMall'
+import Classify from '@/components/pages/Classify'
+import Cart from '@/components/pages/Cart'
+import Me from '@/components/pages/Me'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [{
-    path: '/',
-    name: 'ShoppingMall',
-    component: ShoppingMall
-  }]
+      path: '/',
+      name: 'ShoppingMall',
+      component: ShoppingMall
+    },
+    {
+      path: '/classify',
+      name: 'Classify',
+      component: Classify
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    },
+    {
+      path: '/me',
+      name: 'Me',
+      component: Me
+    },
+    {
+      path: '*',
+      redirect: '/',
+    },
+  ]
 })
