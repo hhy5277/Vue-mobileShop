1a3e73cfea2abb65c1a5699991479429eac6b122
lxjay <lxjay@163.com>
2018-05-23 14:13:56 +0800

    url封装

---
+++ src/serviceAPI.config.js
@@ -0,0 +1,7 @@
+const BASEURL = "https://www.easy-mock.com/mock/5af3a581656ea22f99b56083/mobileShop/";
+const URL = {
+  getShoppingMallInfo: BASEURL + 'index',
+  getGoodsInfo: BASEURL + 'getGoodsInfo'
+}
+
+module.exports = URL
