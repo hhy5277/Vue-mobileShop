a6803b7540a94c7bafa8e519355ed14d2478fda5
lxjay <lxjay@163.com>
2018-06-11 13:41:35 +0800

    配置注册页前后端通讯

--- service/appApi/user.js
+++ service/appApi/user.js
@@ -1,12 +1,13 @@
 const Router = require('koa-router')
 
 let router = new Router()
-router.get('/',async(ctx)=>{
-    ctx.body="这是用户操作首页"
+router.get('/', async (ctx) => {
+  ctx.body = "这是用户操作首页"
 })
 
-router.get('/register',async(ctx)=>{
-    ctx.body="用户注册接口"
+router.get('/register', async (ctx) => {
+  console.log(ctx.request.body)
+  ctx.body = ctx.request.body
 })
 
-module.exports =router
+module.exports = router
