db20b800652bca2a24bc2bae552474363895cddf
lxjay <lxjay@163.com>
2018-06-11 13:34:37 +0800

    koa-bodyparser前后端通讯

--- src/serviceAPI.config.js
+++ src/serviceAPI.config.js
@@ -1,7 +1,9 @@
 const BASEURL = "https://www.easy-mock.com/mock/5ae2eeb23fbbf24d8cd7f0b6/SmileVue/"
-const URL ={
-    getShopingMallInfo : BASEURL+'index',    //商城首页所有信息
-    getGoodsInfo : BASEURL+'getGoodsInfo',
+const LOCALURL = 'http://localhost:3000/'
+const URL = {
+  getShopingMallInfo: BASEURL + 'index', //商城首页所有信息
+  getGoodsInfo: BASEURL + 'getGoodsInfo',
+  registerUser: LOCALURL + 'user/register'
 }
 
 module.exports = URL
