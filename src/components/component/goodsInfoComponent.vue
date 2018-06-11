451c792f85b5c0f8453bc467ea2656f9aa7a700b
lxjay <lxjay@163.com>
2018-06-11 13:43:07 +0800

    热卖商品组件

--- src/components/component/goodsInfoComponent.vue
+++ src/components/component/goodsInfoComponent.vue
@@ -9,22 +9,22 @@
 </template>
 
 <script>
-    import {toMoney}  from '@/filter/moneyFilter.js'
-    export default {
-        props:['goodsImage','goodsName','goodsPrice'],
-        filters:{
-            moneyFilter(money){
-                return toMoney(money)
-            }
-        }
+import { toMoney } from "@/filter/moneyFilter.js";
+export default {
+  props: ["goodsImage", "goodsName", "goodsPrice"],
+  filters: {
+    moneyFilter(money) {
+      return toMoney(money);
     }
+  }
+};
 </script>
 
 <style scoped>
- .goods-name{
-        padding: 0 8px;
-       overflow: hidden;
-       text-overflow: ellipsis;
-       white-space:nowrap; 
-    }
+.goods-name {
+  padding: 0 8px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
 </style>
