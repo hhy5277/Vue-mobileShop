9d44efe09b77f5ad196ea86ac0bdcf1e1a316344
lxjay <lxjay@163.com>
2018-06-19 10:49:47 +0800

    主页

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -20,7 +20,7 @@
                     <img v-lazy="banner.image" width="100%"/>
                 </van-swipe-item>
             </van-swipe>
-        </div>  
+        </div>
         <!--type bar-->
         <div class="type-bar">
             <div v-for="(cate,index) in category" :key="index">
@@ -28,7 +28,7 @@
                 <span>{{cate.mallCategoryName}}</span>
 
             </div>
-        </div>  
+        </div>
         <!--adbanner area-->
         <div>
             <img v-lazy="adBanner" width="100%" />
@@ -46,7 +46,7 @@
                            <div>{{item.goodsName}}</div>
                            <div>￥{{item.price | moneyFilter}}(￥{{item.mallPrice  | moneyFilter}})</div>
                        </div>
-                   </swiper-slide> 
+                   </swiper-slide>
                 </swiper>
             </div>
         </div>
@@ -62,155 +62,143 @@
                     <van-row gutter="20">
                         <van-col span="12" v-for="(item , index) in hotGoods" :key="index">
                                 <goods-info :goodsImage="item.image" :goodsName="item.name" :goodsPrice="item.price">
-
                                 </goods-info>
                         </van-col>
                     </van-row>
                 </van-list>
             </div>
         </div>
-        
-       
-        
     </div>
 </template>
 
 <script>
-    import axios from 'axios'
-    import 'swiper/dist/css/swiper.css'
-    import {swiper , swiperSlide} from 'vue-awesome-swiper'
- 
-    import floorComponent from '../component/floorComponent'
-    import { toMoney } from '@/filter/moneyFilter.js'
-    import goodsInfo from '../component/goodsInfoComponent'
-    import url from '@/serviceAPI.config.js'
-   
-    export default {
-        data() {
-            return {
-                swiperOption:{
-                    slidesPerView:3
-                },
-                msg: 'Shopping Mall',
-                locationIcon: require('../../assets/images/location.png'),
-                bannerPicArray:[],
-                category:[],
-                adBanner:'',
-                recommendGoods:[],
-                floor1:[],
-                floor2:[],
-                floor3:[],
-                floorName:{},
-                hotGoods:[],  //热卖商品
-              
-            }
-        },
-        filters:{
-            moneyFilter(money){
-                return toMoney(money)
-            }
-        },
-        components:{swiper,swiperSlide,floorComponent,goodsInfo},
-        created(){
-            axios({
-                url:url.getShopingMallInfo,
-                method:'get',
-            })
-            .then(response=>{
-                console.log(response)
-                if(response.status==200){
-                    this.category=response.data.data.category;
-                    this.adBanner = response.data.data.advertesPicture.PICTURE_ADDRESS;
-                    this.bannerPicArray= response.data.data.slides;
-                    this.recommendGoods = response.data.data.recommend;
-                    this.floor1 = response.data.data.floor1;
-                    this.floor2 = response.data.data.floor2;
-                    this.floor3 = response.data.data.floor3;
-                    this.floorName = response.data.data.floorName;
-                    this.hotGoods = response.data.data.hotGoods;
+import axios from "axios";
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
 
-                   
-                   
-            
-                }
-            })
-            .catch(error=>{
-                console.log(error)
-            })
-        }
-       
+import floorComponent from "../component/floorComponent";
+import { toMoney } from "@/filter/moneyFilter.js";
+import goodsInfo from "../component/goodsInfoComponent";
+import url from "@/serviceAPI.config.js";
+
+export default {
+  data() {
+    return {
+      swiperOption: {
+        slidesPerView: 3
+      },
+      msg: "Shopping Mall",
+      locationIcon: require("../../assets/images/location.png"),
+      bannerPicArray: [],
+      category: [],
+      adBanner: "",
+      recommendGoods: [],
+      floor1: [],
+      floor2: [],
+      floor3: [],
+      floorName: {},
+      hotGoods: [] //热卖商品
+    };
+  },
+  filters: {
+    moneyFilter(money) {
+      return toMoney(money);
     }
+  },
+  components: { swiper, swiperSlide, floorComponent, goodsInfo },
+  created() {
+    axios({
+      url: url.getShopingMallInfo,
+      method: "get"
+    })
+      .then(response => {
+        console.log(response);
+        if (response.status == 200) {
+          this.category = response.data.data.category;
+          this.adBanner = response.data.data.advertesPicture.PICTURE_ADDRESS;
+          this.bannerPicArray = response.data.data.slides;
+          this.recommendGoods = response.data.data.recommend;
+          this.floor1 = response.data.data.floor1;
+          this.floor2 = response.data.data.floor2;
+          this.floor3 = response.data.data.floor3;
+          this.floorName = response.data.data.floorName;
+          this.hotGoods = response.data.data.hotGoods;
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+};
 </script>
 
 <style scoped>
-    .search-bar{
-        height:2.2rem;
-        background-color: #e5017d;
-        line-height: 2.2rem;
-        overflow: hidden;
-    }
-    .search-input{
-        width:100%;
-        height: 1.3rem;
-        border-top:0px;
-        border-left:0px;
-        border-right:0px;
-        border-bottom:1px solid #fff !important;
-        background-color: #e5017d;
-        color:#fff;
-    }
-    .location-icon{
-        padding-top:.2rem;
-        padding-left:.3rem;
-    }
-    .swiper-area{
-        clear:both;
-        max-height:15rem;
-        overflow: hidden;
-    }
+.search-bar {
+  height: 2.2rem;
+  background-color: #e5017d;
+  line-height: 2.2rem;
+  overflow: hidden;
+}
+.search-input {
+  width: 100%;
+  height: 1.3rem;
+  border-top: 0px;
+  border-left: 0px;
+  border-right: 0px;
+  border-bottom: 1px solid #fff !important;
+  background-color: #e5017d;
+  color: #fff;
+}
+.location-icon {
+  padding-top: 0.2rem;
+  padding-left: 0.3rem;
+}
+.swiper-area {
+  clear: both;
+  max-height: 15rem;
+  overflow: hidden;
+}
 
-    .type-bar{
-        background-color: #fff;
-        margin:0 .3rem .3rem .3rem;
-        border-radius: .3rem;
-        font-size:14px;
-        display:flex;
-        flex-direction:row;
-        flex-wrap:nowrap;
-    }
-    .type-bar div{
-        padding:.3rem;
-        font-size:12px;
-        text-align: center;
-    }
-    .recommend-area{
-        background-color: #fff;
-        margin-top:.3rem;
-    }
-    .recommend-title{
-        border-bottom:1px solid #eee;
-        font-size:14px;
-        padding:.2rem;
-        color:#e5017d;
-    }
-    .recommend-body{
-        border-bottom:1px solid #eee;
-    }
-    .recommend-item{
-        width:99%;
-        border-right:1px solid #eee;
-        font-size:12px;
-        text-align: center;
-    }
-    .hot-area{
-        text-align: center;
-        font-size:14px;
-        height: 1.8rem;
-        line-height:1.8rem;
-    }
-    
-
-
-
-
+.type-bar {
+  background-color: #fff;
+  margin: 0 0.3rem 0.3rem 0.3rem;
+  border-radius: 0.3rem;
+  font-size: 14px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+}
+.type-bar div {
+  padding: 0.3rem;
+  font-size: 12px;
+  text-align: center;
+}
+.recommend-area {
+  background-color: #fff;
+  margin-top: 0.3rem;
+}
+.recommend-title {
+  border-bottom: 1px solid #eee;
+  font-size: 14px;
+  padding: 0.2rem;
+  color: #e5017d;
+}
+.recommend-body {
+  border-bottom: 1px solid #eee;
+}
+.recommend-item {
+  width: 99%;
+  border-right: 1px solid #eee;
+  font-size: 12px;
+  text-align: center;
+}
+.hot-title {
+  color: #f27508;
+}
+.hot-area {
+  text-align: center;
+  font-size: 14px;
+  height: 1.8rem;
+  line-height: 1.8rem;
+}
 </style>
