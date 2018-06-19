b849cb04e5191cab6ab29deb7dad4818bb359b8f
lxjay <lxjay@163.com>
2018-06-19 09:21:02 +0800

    登录服务端逻辑

--- service/appApi/user.js
+++ service/appApi/user.js
@@ -2,28 +2,57 @@ const Router = require('koa-router')
 const mongoose = require('mongoose')
 
 let router = new Router()
-router.get('/', async (ctx) => {
-  ctx.body = "这是用户操作首页"
+router.get('/',async(ctx)=>{
+    ctx.body="这是用户操作首页"
 })
 
-router.post('/register', async (ctx) => {
-  console.log(ctx.request.body)
-  // 获取model
-  const User = mongoose.model('User')
-  //把从前端接收的POST数据封装成一个新的user对象
-  let newUser = new User(ctx.request.body);
-  //用mongoose的save方法直接存储，然后判断是否成功，返回相应的结果
-  await newUser.save().then(() => {
-    ctx.body = {
-      code: 200,
-      message: '注册成功'
-    }
-  }).catch((error) => {
-    ctx.body = {
-      code: 500,
-      message: error
-    }
-  });
+router.post('/register',async(ctx)=>{
+   
+    const User = mongoose.model('User')
+    let newUser = new User(ctx.request.body)
+
+    await newUser.save().then(()=>{
+        ctx.body={
+            code:200,
+            message:'注册成功'
+        }
+    }).catch(error=>{
+        ctx.body={
+            code:500,
+            message:error
+        }
+    })
 })
 
-module.exports = router
+router.post('/login',async(ctx)=>{
+    let loginUser = ctx.request.body
+    console.log(loginUser)
+    let userName = loginUser.userName
+    let password = loginUser.password
+
+    //引入User的model
+    const User = mongoose.model('User')
+
+    await User.findOne({userName:userName}).exec().then(async(result)=>{
+        console.log(result)
+        if(result){
+            let newUser = new User()
+            await newUser.comparePassword(password,result.password)
+            .then(isMatch=>{
+                ctx.body={code:200,message:isMatch}
+            })
+            .catch(error=>{
+                console.log(error)
+                ctx.body={code:500,message:error}
+            })
+        }else{
+            ctx.body={code:200,message:'用户名不存在'}
+        }
+    }).catch(error=>{
+        console.log(error)
+        ctx.body={code:500,message:error}
+    })
+
+})
+
+module.exports =router
