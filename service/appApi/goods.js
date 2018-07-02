9d483afddcc00c11f18be7c4985d4932a0f9dd26
lxjay@163.com <237768986@qq.com>
2018-07-02 09:49:11 +0800

    修改商品详情信息接口

--- service/appApi/goods.js
+++ service/appApi/goods.js
@@ -44,16 +44,14 @@ router.get('/insertAllCategory',async(ctx)=>{
 });
 //***获取商品详细信息的接口
 router.post('/getDetailGoodsInfo',async(ctx)=>{
-    let goodsId = ctx.request.body.goodsId
-    const Goods = mongoose.model('Goods')
-    await Goods.findOne({ID:goodsId}).exec()
-    .then(async(result)=>{
+    try{
+        let goodsId = ctx.request.body.goodsId
+        const Goods = mongoose.model('Goods')
+        let result=await Goods.findOne({ID:goodsId}).exec()
         ctx.body={code:200,message:result}
-    })
-    .catch(error=>{
-        console.log(error)
-        ctx.body={code:500,message:error}
-    })
+    }catch(err){
+        ctx.body={code:500,message:err}
+    }
 
 })
 
