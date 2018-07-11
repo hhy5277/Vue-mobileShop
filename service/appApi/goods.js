c421fb8303791762255bb04287d4ae5bd7f2250e
lxjay@163.com <237768986@qq.com>
2018-07-11 09:38:09 +0800

    更新商品类别的接口

--- service/appApi/goods.js
+++ service/appApi/goods.js
@@ -74,6 +74,42 @@ router.post('/getDetailGoodsInfo',async(ctx)=>{
         ctx.body={code:500,message:error}
     }
 })
+//**获取类别的接口
+router.get('/getCategoryList',async(ctx)=>{
+    try{
+        const Category = mongoose.model('Category')
+        let result = await Category.find().exec()
+        ctx.body={code:200,message:result}
+    }catch(err){
+        ctx.body={code:500,message:err}
+    }
+
+})
+//**获取类别的小接口
+router.get('/getCategorySubList',async(ctx)=>{
+    try{
+        //let categoryId = ctx.request.body.categoryId
+        let categoryId = 1
+        const CategorySub = mongoose.model('CategorySub')
+        let result = await CategorySub.find({MALL_CATEGORY_ID:categoryId}).exec()
+        ctx.body={code:200,message:result}
+    }catch(err){
+        ctx.body={code:500,message:err}
+    }
+})
+// 根据商品类别获取商品列表
+router.get('/getGoodsListByCategorySubID',async(ctx)=>{
+    try{
+        //let categorySubId = ctx.request.body.categoryId
+        let categorySubId = '2c9f6c946016ea9b016016f79c8e0000'
+        const Goods = mongoose.model('Goods')
+        let result = await Goods.find({SUB_ID:categorySubId}).exec()
+        ctx.body={code:200,message:result}
+    }catch(err){
+        ctx.body={code:500,message:err}
+    }
+
+})
 
 
 
