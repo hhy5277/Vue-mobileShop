a7aee8aec02cb1b6f2784ed86db159aa0a4f55f4
lxjay@163.com <237768986@qq.com>
2018-07-15 14:22:03 +0800

    配置大类商品相关逻辑

--- src/serviceAPI.config.js
+++ src/serviceAPI.config.js
@@ -1,11 +1,12 @@
 const BASEURL = "https://www.easy-mock.com/mock/5ae2eeb23fbbf24d8cd7f0b6/SmileVue/"
 const LOCALURL = "http://localhost:3000/"
-const URL ={
-    getShopingMallInfo : BASEURL+'index',    //商城首页所有信息
-    getGoodsInfo : BASEURL+'getGoodsInfo', 
-    registerUser : LOCALURL+'user/register',  //用户注册接口
-    login : LOCALURL+'user/login',  //用户登录接口
-    getDetailGoodsInfo : LOCALURL+'goods/getDetailGoodsInfo',  //获取商品详情
+const URL = {
+  getShopingMallInfo: BASEURL + 'index', //商城首页所有信息
+  getGoodsInfo: BASEURL + 'getGoodsInfo',
+  registerUser: LOCALURL + 'user/register', //用户注册接口
+  login: LOCALURL + 'user/login', //用户登录接口
+  getDetailGoodsInfo: LOCALURL + 'goods/getDetailGoodsInfo', //获取商品详情
+  getCategoryList: LOCALURL + 'goods/getCategoryList', //得到大类信息
 }
 
 module.exports = URL
