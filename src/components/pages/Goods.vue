99762320092a58eb35f443655d6fdfd0e70f1f39
lxjay@163.com <237768986@qq.com>
2018-08-02 09:51:25 +0800

    添加购物车完成

--- src/components/pages/Goods.vue
+++ src/components/pages/Goods.vue
@@ -17,7 +17,7 @@
             <van-tabs swipeable sticky>
                 <van-tab title="商品详情">
                    <div class="detail" v-html="goodsInfo.DETAIL">
-                      
+
 
                    </div>
                 </van-tab>
@@ -30,7 +30,7 @@
 
         <div class="goods-bottom">
             <div>
-                <van-button size="large" type="primary">加入购物车</van-button>
+                <van-button size="large" type="primary" @click="addGoodsToCart">加入购物车</van-button>
             </div>
             <div>
                 <van-button size="large" type="danger">直接购买</van-button>
@@ -41,77 +41,105 @@
 </template>
 
 <script>
-    import axios from 'axios'
-    import url from '@/serviceAPI.config.js'
-    import {Toast} from 'vant'
-    import {toMoney} from '@/filter/moneyFilter.js'
-    export default {
-        data() {
-            return {
-                goodsId:'',
-                goodsInfo:{},  //商品详细信息 
-            }
-        },
-        filters:{
-            moneyFilter(money){
-                return toMoney(money)
-            }
-        },
-        created(){
-            this.goodsId= this.$route.query.goodsId ?this.$route.query.goodsId : this.$route.params.goodsId
-            console.log(this.goodsId)
-            this.getInfo()
-        },
-        methods: {
-            getInfo() {
-                axios({
-                    url:url.getDetailGoodsInfo,
-                    method:'post',
-                    data:{goodsId:this.goodsId}
-                })
-                .then(response=>{
-                   
-                    if(response.data.code== 200 && response.data.message){
-                        this.goodsInfo= response.data.message
-                    }else{
-                        Toast('服务器错误，数据获取失败')
-                    }
-                     console.log(this.goodsInfo)
-                })
-                .catch(error=>{
-                    console.log(error)
-                })
-            },
-            onClickLeft(){
-                this.$router.go(-1)
-            }
-        },
+import axios from "axios";
+import url from "@/serviceAPI.config.js";
+import { Toast } from "vant";
+import { toMoney } from "@/filter/moneyFilter.js";
+export default {
+  data() {
+    return {
+      goodsId: "",
+      goodsInfo: {} //商品详细信息
+    };
+  },
+  filters: {
+    moneyFilter(money) {
+      return toMoney(money);
     }
+  },
+  created() {
+    this.goodsId = this.$route.query.goodsId
+      ? this.$route.query.goodsId
+      : this.$route.params.goodsId;
+    console.log(this.goodsId);
+    this.getInfo();
+  },
+  methods: {
+    getInfo() {
+      axios({
+        url: url.getDetailGoodsInfo,
+        method: "post",
+        data: { goodsId: this.goodsId }
+      })
+        .then(response => {
+          if (response.data.code == 200 && response.data.message) {
+            this.goodsInfo = response.data.message;
+          } else {
+            Toast("服务器错误，数据获取失败");
+          }
+          console.log(this.goodsInfo);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    onClickLeft() {
+      this.$router.go(-1);
+    }
+  },
+  addGoodsToCart() {
+    // 取出购物车内的商品数据
+    let cartInfo = localStorage.cartInfo
+      ? JSON.parse(localStorage.cartInfo)
+      : [];
+    let isHaveGoods = cartInfo.find(cart => cart.goodsId == this.goodsId);
+    console.log(isHaveGoods);
+    if (!isHaveGoods) {
+      //  没有商品直接添加到数组
+      // 重新组成添加到购物车的信息
+      let newGoodsInfo = {
+        goodsId: this.goodsInfo.ID,
+        Name: this.goodsInfo.Name,
+        price: this.goodsInfo.PRESULT_PRICE,
+        image: this.goodsInfo.IAMGE1,
+        count: 1
+      };
+      // 添加购物车
+      cartInfo.push(newGoodsInfo);
+      // 操作本地数据
+      localStorage.cartInfo = JSON.stringify(cartInfo);
+      Toast.success("添加成功");
+    } else {
+      Toast.success("已有此商品");
+    }
+    this.$router.push({ path: "Cart" });
+  }
+};
 </script>
 
 <style scoped>
-    .goods-name{
-        background-color: #fff;
-    } 
-    .goods-price{
-        background-color: #fff;
-    }
-    .detail {
-        font-size:0px;
-    }
-    .goods-bottom{
-        position:fixed;
-        bottom:0px;
-        left:0px;
-        background-color: #FFF;
-        width:100%;
-        display: flex;
-        flex-direction: row;
-        flex-flow:nowrap;
-    }
+.goods-name {
+  background-color: #fff;
+}
+.goods-price {
+  background-color: #fff;
+}
+.detail {
+  font-size: 0px;
+}
+.goods-bottom {
+  position: fixed;
+  bottom: 0px;
+  left: 0px;
+  background-color: #fff;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-flow: nowrap;
+}
 
-    .goods-bottom > div {
-        flex:1;
-        padding:5px;
-    }
+.goods-bottom > div {
+  flex: 1;
+  padding: 5px;
+}
 </style>
