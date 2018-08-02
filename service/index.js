13b33e57f1706a12b9acc7a371cb7dcecfe2d001
lxjay@163.com <237768986@qq.com>
2018-08-02 15:18:24 +0800

    koa2中间件配置

--- service/index.js
+++ service/index.js
@@ -1,17 +1,13 @@
 const Koa = require('koa')
 const app = new Koa()
-const { connect , initSchemas } = require('./database/init.js')
+const Router = require('koa-router')
 const mongoose = require('mongoose')
+const { connect, initSchemas} = require('./database/init.js')
 const bodyParser = require('koa-bodyparser')
 const cors = require('koa2-cors')
-const Router = require('koa-router')
-
 
 app.use(bodyParser())
 app.use(cors())
-
-
-
 
 let user = require('./appApi/user.js')
 let home = require('./appApi/home.js')
@@ -22,24 +18,20 @@ let router = new Router()
 router.use('/user',user.routes())
 router.use('/home',home.routes())
 router.use('/goods',goods.routes())
-//egg.js
 
 //加载路由中间件
-
 app.use(router.routes())
 app.use(router.allowedMethods())
-
-
-;(async ()=>{
-    await connect()
-    initSchemas()
+;(async () => {
+  await connect()
+  initSchemas()
 })()
 
 
-app.use(async(ctx)=>{
-    ctx.body='<h1>Hello Koa2</h1>'
+app.use(async (ctx) => {
+  ctx.body = '<h1>Hello Koa2</h1>'
 })
 
-app.listen(3000,()=>{
-    console.log('[Server] starting at port 3000')
+app.listen(3000, () => {
+  console.log('[Server] starting at port 3000')
 })
