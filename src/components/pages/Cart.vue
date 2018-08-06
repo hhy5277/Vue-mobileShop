5bf86217f17847e07d0cf32ffb8b8bccfae7c91c
lxjay@163.com <237768986@qq.com>
2018-08-06 10:53:10 +0800

    购物车-完善商品计算/清空购物车功能

--- src/components/pages/Cart.vue
+++ src/components/pages/Cart.vue
@@ -1,12 +1,53 @@
 <template>
     <div>
         <div class="navbar-div">
-            <van-nav-bar  title="购物车"></van-nav-bar>
+            <van-nav-bar
+                title="购物车"
+                left-text="返回"
+                left-arrow
+                @click-left="onClickLeft"/>
+        </div>
+        <!--显示购物车中的商品-->
+        <div class="cart-list">
+            <div class="cart-row" v-for="(item,index) in cartInfo" :key="index">
+                <div class="cart-img">
+                    <img :src="item.image" width="100%" />
+                </div>
+                <div class="cart-text">
+                    <div class="cart-goods-name">{{item.name}}</div>
+                    <div class="cart-control">
+                        <van-stepper v-model="item.count" />
+                    </div>
+                </div>
+                <div class="cart-goods-price">
+                    <div>
+                        ￥{{item.price | moneyFilter}}
+                    </div>
+                    <div>
+                        x{{item.count}}
+                    </div>
+                    <div class="allPrice">
+                        ￥{{item.price*item.count | moneyFilter}}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--显示总金额-->
+        <div class="totalMoeny">
+            商品总价:￥{{totalMoney | moneyFilter}}
+        </div>
+        <div class="cart-title">
+            <van-button size="small"  type="danger" @click="clearCart" plain>
+                清空购物车
+            </van-button>
         </div>
     </div>
 </template>
 
+
+
 <script>
+import { toMoney } from "@/filter/moneyFilter.js";
 export default {
   data() {
     return {
@@ -14,8 +55,26 @@ export default {
       isEmpty: false // 购物车是否为空，空为false,为空显示false
     };
   },
+  // 调用获取购物车商品方法
   created() {
     this.getCartInfo();
+  },
+  //  计算商品总额
+  computed: {
+    totalMoney() {
+      let allMoney = 0;
+      this.cartInfo.forEach((item, index) => {
+        allMoney += item.price * item.count;
+      });
+      localStorage.cartInfo = JSON.stringify(this.cartInfo);
+      return allMoney;
+    }
+  },
+  // 价格过滤器
+  filters: {
+    moneyFilter(money) {
+      return toMoney(money);
+    }
   },
   methods: {
     // 获取购物车内的商品
@@ -28,10 +87,61 @@ export default {
       // 打印得到结果
       console.log("this.cartInfo" + JSON.stringify(this.cartInfo));
       this.isEmpty = this.cartInfo.length > 0 ? true : false;
+    },
+    //清空购物车的商品
+    clearCart() {
+      localStorage.removeItem("cartInfo");
+      this.cartInfo = [];
+    },
+    // 返回上层路径
+    onClickLeft() {
+      this.$router.replace({ path: "/" });
     }
   }
 };
 </script>
 
-<style>
+<style scoped>
+.cart-title {
+  height: 2rem;
+  line-height: 2rem;
+  background-color: #fff;
+  border-bottom: 1px solid #e4e7ed;
+  padding: 5px;
+  text-align: right;
+}
+.cart-list {
+  background-color: #fff;
+}
+.cart-row {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  padding: 0.5rem;
+  font-size: 0.85rem;
+  border-bottom: 1px solid #e4e7ed;
+}
+.cart-img {
+  flex: 6;
+}
+.cart-text {
+  flex: 14;
+  padding-left: 10px;
+}
+.cart-control {
+  padding-top: 10px;
+}
+.cart-goods-price {
+  flex: 4;
+  text-align: right;
+}
+.allPrice {
+  color: red;
+}
+.totalMoeny {
+  text-align: right;
+  background-color: #fff;
+  border-bottom: 1px solid #e4e7ed;
+  padding: 5px;
+}
 </style>
