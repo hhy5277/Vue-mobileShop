11c0a9336b4f3c773dd43626533f60f6dfdf1374
lxjay@163.com <237768986@qq.com>
2018-08-08 09:51:48 +0800

    主页

--- src/components/pages/Main.vue
+++ src/components/pages/Main.vue
@@ -7,7 +7,7 @@
             <van-tabbar-item icon='shop'>首页</van-tabbar-item>
             <van-tabbar-item icon='records'>列表</van-tabbar-item>
             <van-tabbar-item icon='cart'>购物车</van-tabbar-item>
-            <van-tabbar-item icon='contact' info='20'>会员中心</van-tabbar-item>
+            <van-tabbar-item icon='contact'>会员中心</van-tabbar-item>
         </van-tabbar>
     </div>
 </template>
@@ -18,6 +18,10 @@ export default {
     return {
       active: 0
     };
+  },
+  created() {
+    // 调用判断路径的方法
+    this.changeTabBarActive();
   },
   methods: {
     changeTabbar(active) {
@@ -34,6 +38,20 @@ export default {
           break;
         case 3:
           break;
+      }
+    },
+    // 判断路径
+    changeTabBarActive() {
+      this.nowPath = this.$route.path;
+      if (this.nowPath == "/") {
+          console.log('执行首页')
+        this.active = 0;
+      } else if (this.nowPath == "/CategoryList") {
+          console.log('执行分类')
+        this.active = 1;
+      } else if (this.nowPath == "/Cart") {
+          console.log('执行购物车')
+        this.active = 2;
       }
     }
   }
