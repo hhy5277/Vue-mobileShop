0e32b708efa1c76f8a36163be29c6da7bcd56912
lxjay@163.com <237768986@qq.com>
2018-08-14 14:12:06 +0800

    注册页

--- src/components/pages/Register.vue
+++ src/components/pages/Register.vue
@@ -49,7 +49,7 @@ export default {
   },
   methods: {
     goBack() {
-      this.$router.go(-1);
+      this.$router.replace({ name: "Member" });
     },
     registerAction() {
       this.checkForm() && this.axiosRegisterUser();
